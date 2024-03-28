c83a579671ef11e3bc95638d46794e27e965e7bd
ImmaculeeNamahirwe <i.namahirwe@alustudent.com>
2024-03-28 11:29:58 +0200

    myportfolio

---
+++ p.py
@@ -0,0 +1,25 @@
+def my_fct(*args, **kwargs):
+    print("{} - {}".format(args, kwargs))
+
+my_fct() # () - {}
+my_fct("Best") # ('Best',) - {}
+my_fct("Best", 89) # ('Best', 89) - {}
+my_fct(name="Best") # () - {'name': 'Best'}
+my_fct(name="Best", number=89) # () - {'name': 'Best', 'number': 89}
+my_fct("School", 12, name="Best", number=89) # ('School', 12) - {'name': 'Best', 'number': 89}
+
+
+def my_fct(*args, **kwargs):
+    print("{} - {}".format(args, kwargs))
+
+a_dict = { 'name': "Best", 'age': 89 }
+
+my_fct(a_dict) # ({'age': 89, 'name': 'Best'},) - {}
+my_fct(*a_dict) # ('age', 'name') - {}
+my_fct(**a_dict) # () - {'age': 89, 'name': 'Best'}
+
+from datetime import datetime
+
+date_now = datetime.now()
+print(type(date_now)) # <class 'datetime.datetime'>
+print(date_now) # 2017-06-08 20:42:42.170922
